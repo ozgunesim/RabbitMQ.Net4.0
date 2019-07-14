8f0171ef00a5be4533fc26e94e098e58be2e418d
ozgunesim <ozgunesim@gmail.com>
2019-07-15 02:54:31 +0300

    v1.0

---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQNet40
+{
+    public partial class Form1 : Form
+    {
+        IModel channel;
+        IConnection conn;
+        EventingBasicConsumer consumer;
+        string queueName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            // "guest"/"guest" by default, limited to localhost connections
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.VirtualHost = "/";
+            factory.HostName = "localhost";
+            factory.Port = 5672;
+
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+            channel.ExchangeDeclare("servio_test", "fanout");
+
+            queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "servio_test",
+                              routingKey: "");
+
+            consumer = new EventingBasicConsumer(channel);
+            //consumer.Received += Consumer_Received;
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                //MessageBox.Show(message.ToString());
+                Console.WriteLine(message.ToString());
+                textBox1.Invoke((MethodInvoker)delegate {
+                    // Running on the UI thread
+                    textBox1.Text += message.ToString() + Environment.NewLine;
+                });
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                 noAck: true,
+                                 consumer: consumer);
+
+
+        }
+        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            channel.Close();
+            conn.Close();
+        }
+    }
+}
---
+++ Publisher/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    public partial class Form1 : Form
+    {
+        IConnection connection;
+        IModel channel;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "servio_test", type: "fanout");
+
+                
+            
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var message = textBox1.Text;
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "servio_test",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
